75cf6a9fbb141a184739b40a2180c0d49a682816
nishantraj656 <nishantraj656@gmail.com>
2019-01-20 21:49:18 +0530

    Master

--- Grocery.php
+++ Grocery.php
@@ -8,23 +8,14 @@
 	Route::post('gro_product','API\Groceries\CategoryController@productGet');
 	Route::post('gro_product_shop','API\Groceries\CategoryController@RelatedShopsGet');
 	
-	
-	
+	/**Order List*/
+	Route::post('gro_order','API\Groceries\OrderSet@getRequestOrderValue');
+
+	Route::get('Grocery/Order','API\Groceries\OrderSet@getOrderList');
 	
 	/** Particular shop */
 	Route::post('Grocery/Shop/category','API\Groceries\ShopController@categoryGet');
 	Route::post('Grocery/Shop/sub','API\Groceries\ShopController@subCategoryGet');
 	Route::post('Grocery/Shop/productList','API\Groceries\ShopController@productGet');
 	Route::get('Grocery/Shop/List','API\Groceries\ShopController@shopInfoGet');
-	Route::post('Grocery/Shop/product/price','API\Groceries\ShopController@productPriceGet');
-
-	Route::group(['middleware' => 'auth:api'], function(){
-		
-		Route::post('gro_order','API\Groceries\OrderSet@getRequestOrderValue');
-		Route::post('Grocery/Order/History','API\Groceries\OrderSet@getOrderList');
-		Route::post('Grocery/Order/History/Item','API\Groceries\OrderSet@getOrderListItem');
-		
-	});
-	
-	
 ?>
--- app/Http/Controllers/API/Groceries/LoginSignUP.php
+++ app/Http/Controllers/API/Groceries/LoginSignUP.php
@@ -52,26 +52,34 @@ class LoginSignUP extends Controller
     */ 
     public function register(Request $request) 
     { 
-        $name = $request->json()->all()['name'];
+       /** $name = $request->json()->all()['name'];
         $email = $request->json()->all()['email'];
         // $password = $request->json()->all()['password'];
         // $c_password = $request->json()->all()['c_password'];
         $phone = $request->json()->all()['phone'];
-        $user_type = $request->json()->all()['user_type'];
+        $user_type = $request->json()->all()['user_type'];*/
+		$name = $request->name;
+		$email = $request->email;
+		$password = $request->password;
+		$c_password = $request->c_password;
+		$phone = $request->phone;
+		$user_type = $request->user_type;
         
 
         // saving login data to user table 
 
-        $validator = Validator::make($request->json()->all(), [ 
+        $validator = Validator::make($request->json()->all(),[ 
             'name' => 'required', 
             'email' => 'required|email', 
             'password' => 'required', 
             'c_password' => 'required|same:password', 
             'phone' => 'required|numeric',
         ]);
+		
         if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
+		
         $input = $request->json()->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
@@ -83,7 +91,7 @@ class LoginSignUP extends Controller
 
 
         //fetching user id of the inserted data in users table 
-        $wor_info_id = DB::table('users')->select('id')
+        $user_info_id = DB::table('users')->select('id')
                         ->where('email', '=', $email)
                         ->get();
         
