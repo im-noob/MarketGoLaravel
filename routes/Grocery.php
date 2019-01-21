eecbbf7621cb63568d4e7972381fdb2c6911277e
BeeruBihari <Rajbeeru61@gmail.com>
2019-01-21 04:52:42 -0800

    my fiest commit Beeru

--- routes/Grocery.php
+++ routes/Grocery.php
@@ -8,7 +8,10 @@
 	Route::post('gro_product','API\Groceries\CategoryController@productGet');
 	Route::post('gro_product_shop','API\Groceries\CategoryController@RelatedShopsGet');
 	
-	
+	/**Order List*/
+	Route::post('gro_order','API\Groceries\OrderSet@getRequestOrderValue');
+
+	Route::get('Grocery/Order','API\Groceries\OrderSet@getOrderList');
 	
 	
 	/** Particular shop */
@@ -16,20 +19,16 @@
 	Route::post('Grocery/Shop/sub','API\Groceries\ShopController@subCategoryGet');
 	Route::post('Grocery/Shop/productList','API\Groceries\ShopController@productGet');
 	Route::get('Grocery/Shop/List','API\Groceries\ShopController@shopInfoGet');
-	Route::post('Grocery/Shop/product/price','API\Groceries\ShopController@productPriceGet')
+	Route::post('Grocery/Shop/product/price','API\Groceries\ShopController@productPriceGet');
 
+
+	//For Authenticated user.
 	Route::group(['middleware' => 'auth:api'], function(){
-		
 		Route::post('gro_order','API\Groceries\OrderSet@getRequestOrderValue');
 		Route::post('Grocery/Order/History','API\Groceries\OrderSet@getOrderList');
 		Route::post('Grocery/Order/History/Item','API\Groceries\OrderSet@getOrderListItem');
 		
-	});
-
-	
-	//For Authenticated user.
-	Route::group(['middleware' => 'auth:api'], function(){
-		// Route::post('details', 'API\UserController@details')
+		
 		Route::post('profileGR', 'API\Groceries\profile_GR@UpdateProfile');
 		Route::post('Retailer/UpdateOrder', 'API\Groceries\RQuery@UpdateOrder');
 		Route::post('Retailer/getOrder', 'API\Groceries\OrderC@getActiveOrder');
@@ -38,6 +37,5 @@
 
 	Route::get('Retailer/getCatrgory', 'API\Groceries\RQuery@getCategory');
 	Route::post('Retailer/AddCategory', 'API\Groceries\RQuery@AddCategory');
-	
 	
 ?>
