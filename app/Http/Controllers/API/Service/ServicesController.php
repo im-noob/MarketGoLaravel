b681068e1defc7bc32dbc4432cb3deec999bcaa4
amriteshonly4u <amriteshonly4u@gmail.com>
2019-01-24 22:28:24 +0530

    User Service Home UI all done

--- app/Http/Controllers/API/Service/ServicesController.php
+++ app/Http/Controllers/API/Service/ServicesController.php
@@ -8,37 +8,36 @@ use Illuminate\Support\Facades\DB;
 
 class ServicesController extends Controller
 {
-<<<<<<< HEAD
-     public function categoryGet()
-    {
-	/** SELECT DISTINCT gro_cat_tab.gro_cat_id,gro_cat_tab.gro_cat_name,gro_cat_tab.pic from gro_product_shop_tab  INNER JOIN gro_map_tab ON gro_product_shop_tab.gro_map_id = gro_map_tab.gro_map_id  INNER JOIN gro_cat_tab ON gro_map_tab.gro_cat_id = gro_cat_tab.gro_cat_id */
-      // $data = DB::table('gro_cat_tab')->select("gro_cat_id","gro_cat_name","pic")->orderBy('gro_cat_id')->simplePaginate(20);
-	 $data = DB::table('wor_cat_tab')
-		->select("wor_cat_name","wor_cat_id","wor_cat_pic")
-		->distinct()
-		->orderBy('wor_cat_id')
-		->simplePaginate(20);
 
-        return response()->json(['data' => $data]);
-    }
+ //     public function categoryGet()
+ //    {
+	// /** SELECT DISTINCT gro_cat_tab.gro_cat_id,gro_cat_tab.gro_cat_name,gro_cat_tab.pic from gro_product_shop_tab  INNER JOIN gro_map_tab ON gro_product_shop_tab.gro_map_id = gro_map_tab.gro_map_id  INNER JOIN gro_cat_tab ON gro_map_tab.gro_cat_id = gro_cat_tab.gro_cat_id */
+ //      // $data = DB::table('gro_cat_tab')->select("gro_cat_id","gro_cat_name","pic")->orderBy('gro_cat_id')->simplePaginate(20);
+	//  $data = DB::table('wor_cat_tab')
+	// 	->select("wor_cat_name","wor_cat_id","wor_cat_pic")
+	// 	->distinct()
+	// 	->orderBy('wor_cat_id')
+	// 	->simplePaginate(20);
+
+ //        return response()->json(['data' => $data]);
+ //    }
 	
-	/** Subcategory*/
-	  public function subCategoryGet(Request $request)
-    {
-		$catID = $request->id; 
-	/** SELECT DISTINCT gro_cat_tab.gro_cat_id,gro_cat_tab.gro_cat_name,gro_cat_tab.pic from gro_product_shop_tab  INNER JOIN gro_map_tab ON gro_product_shop_tab.gro_map_id = gro_map_tab.gro_map_id  INNER JOIN gro_cat_tab ON gro_map_tab.gro_cat_id = gro_cat_tab.gro_cat_id */
-      // $data = DB::table('gro_cat_tab')->select("gro_cat_id","gro_cat_name","pic")->orderBy('gro_cat_id')->simplePaginate(20);
-	 $data = DB::table('wor_subcat_tab')
-		->select("subcat_name","wor_cat_id","wor_subcat_id","pic")
-		->where("wor_cat_id","=",$catID)
-		->distinct()
-		->orderBy('wor_subcat_id')
-		->simplePaginate(20);
+	// /** Subcategory*/
+	//   public function subCategoryGet(Request $request)
+ //    {
+	// 	$catID = $request->id; 
+	// /** SELECT DISTINCT gro_cat_tab.gro_cat_id,gro_cat_tab.gro_cat_name,gro_cat_tab.pic from gro_product_shop_tab  INNER JOIN gro_map_tab ON gro_product_shop_tab.gro_map_id = gro_map_tab.gro_map_id  INNER JOIN gro_cat_tab ON gro_map_tab.gro_cat_id = gro_cat_tab.gro_cat_id */
+ //      // $data = DB::table('gro_cat_tab')->select("gro_cat_id","gro_cat_name","pic")->orderBy('gro_cat_id')->simplePaginate(20);
+	//  $data = DB::table('wor_subcat_tab')
+	// 	->select("subcat_name","wor_cat_id","wor_subcat_id","pic")
+	// 	->where("wor_cat_id","=",$catID)
+	// 	->distinct()
+	// 	->orderBy('wor_subcat_id')
+	// 	->simplePaginate(20);
 
-        return response()->json(['data' => $data]);
-    }
+ //        return response()->json(['data' => $data]);
+ //    }
 
-=======
     public $successStatus = 200;
 
     public function Cat_SubCat_Json(Request $request) 
@@ -67,7 +66,87 @@ class ServicesController extends Controller
             $intermediateArr["subcategory"] = $subcategory_arr;
             array_push($cat_sub_cat_arr_final, $intermediateArr);
         }
-      	return response()->json(['data'=>$cat_sub_cat_arr_final],$this->successStatus);
+      	return response()->json(['received'=>'yes','data'=>$cat_sub_cat_arr_final],$this->successStatus);
     }
->>>>>>> 1804fdb104a8b709269df502d31c8c70dc810ae0
+    public function ServiceManList_get(Request $request){
+        $subcatID = $request->json()->all()['subcategoryID'];
+        $data = [
+                    [
+                      "info_id"=>"1",
+                      "avtar_url"=>"https://instagram.fpat1-1.fna.fbcdn.net/vp/84c4e443d47dc2aa70a613a017a4c001/5CBB0AAC/t51.2885-19/s150x150/31908285_2109461939310314_4190149362170462208_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net",
+                      "name"=>"Aarav kumar",
+                      "ratting"=>"5.0",
+                      "review"=>"1500",
+                      "rate"=>"1415"
+                    ],
+                    [
+                      "info_id"=>"2",
+                      "avtar_url"=>"https://instagram.fpat1-1.fna.fbcdn.net/vp/836fa6eefe891bacb435221da1a34e9f/5CB68126/t51.2885-19/s150x150/23594988_159717817967684_5705323595526307840_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net",
+                      "name"=>"Sushant Kumar",
+                      "ratting"=>"1.9",
+                      "review"=>"640",
+                      "rate"=>"1100"
+                    ],
+                    [
+                      "info_id"=>"3",
+                      "avtar_url"=>"https://instagram.fpat1-1.fna.fbcdn.net/vp/d9dfa225194e9cbd0a0abfed6f754559/5CD8326A/t51.2885-19/s150x150/40845500_267627190434383_2753863881221734400_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net",
+                      "name"=>"Ritika",
+                      "ratting"=>"4.7",
+                      "review"=>"1200",
+                      "rate"=>"1000"
+                    ],
+                    [
+                      "info_id"=>"4",
+                      "avtar_url"=>"https://instagram.fpat1-1.fna.fbcdn.net/vp/875eed91e71a0354a970d9bbc5adebbd/5CD73B3E/t51.2885-19/s150x150/47173921_268889120486253_4384497285149491200_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net",
+                      "name"=>"Radhika Garg",
+                      "ratting"=>"4.3",
+                      "review"=>"574",
+                      "rate"=>"918"
+                    ]
+                ];
+
+        return response()->json(['received'=>'yes','data'=>$data,'return_test'=>$subcatID],$this->successStatus);
+    }
+    public function renderProfileData_get(Request $request){
+        $wor_serviceManPorifleID = $request->json()->all()['profileID'];
+        $data = [
+                    'avtar'=>'https://instagram.fpat1-1.fna.fbcdn.net/vp/84c4e443d47dc2aa70a613a017a4c001/5CBB0AAC/t51.2885-19/s150x150/31908285_2109461939310314_4190149362170462208_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net',
+                    'name'=>'Aarav Kumar',
+                    'searched'=>'100',
+                    'contract'=>'3000',
+                    'ratting'=>'4.5',
+                    'startHour'=>'10:00',
+                    'endHour'=>'04:00',
+                    'experi'=>'5',
+                    'addList'=>[
+                        [   'key'=>'State','value'=>'Bihar'],
+                        [   'key'=>'City','value'=>'Bhagalpur'],
+                        [   'key'=>'Pincode','value'=>'812002'],
+                        [   'key'=>'Address','value'=>'Room no:3,Block-17,South Colony,Nayabazar,Bhagalpur'],
+                    ],
+                    'workList' => [
+                        [   'key'=>'Temp Reparing','value'=>'4000-8524' , 'workSubCatId'=>'6'],
+                        [   'key'=>'Computer Reparing','value'=>'500-855', 'workSubCatId'=>'3'],
+                        [   'key'=>'HDD','value'=>'8000-8855', 'workSubCatId'=>'2'],
+                        [   'key'=>'Wirring','value'=>'5150-6520', 'workSubCatId'=>'15'],
+                    ],
+                    'contactNo'=>'1234567890',
+                    'workerID'=>'34',
+                ];
+
+        return response()->json(['received'=>'yes','data'=>$data,'return_test'=>$wor_serviceManPorifleID],$this->successStatus);
+    }
+    public function HierMe_do(Request $request){
+        $wor_info_id = $request->json()->all()['workerID'];
+        $wor_subcat_id = $request->json()->all()['WorkSubCatID'];
+        $customer_info_Id = $request->json()->all()['customerID'];
+        $message = $request->json()->all()['message'];
+        $title = $request->json()->all()['title'];
+
+        $data = [
+                    
+                ];
+
+        return response()->json(['received'=>'yes','data'=>$data,'return_test'=>$request],$this->successStatus);
+    }
 }
