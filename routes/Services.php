534adbea02cf87425d85fedab2714cdf303a1939
amriteshonly4u <amriteshonly4u@gmail.com>
2019-01-26 15:29:38 +0530

    Both UI doine in service user

--- routes/Services.php
+++ routes/Services.php
@@ -8,4 +8,8 @@
 	Route::post('render_ServiceManList_US','API\Service\ServicesController@ServiceManList_get');
 	Route::post('render_renderProfileData_US','API\Service\ServicesController@renderProfileData_get');
 	Route::post('render_HandleSendRequest_HierMe_US','API\Service\ServicesController@HierMe_do');
+
+	// history section
+	Route::post('render_userServiceHist_US','API\Service\ServicesController@ServiceHistory_get');
+	//
  ?>
